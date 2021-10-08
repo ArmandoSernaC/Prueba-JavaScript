836b0d11ff30f26f0d67a24f60475cd819ee6c26
Armando Serna <aa.serna10@uniandes.edu.co>
2021-10-08 02:13:56 -0500

    Se agregó la funcionalidad de registro

--- pruebas.js
+++ pruebas.js
@@ -1,8 +1,11 @@
-var Tregistro = document.getElementById("Tabla-registro");
-var Eregistro = document.getElementById("liveAlertBtn");
-var Pregistro = document.getElementById("reg-form");
-var Pregistro2 = document.getElementById("reg-form2");
-var infodata = document.getElementById("Info-nodata");
+const Tregistro = document.getElementById("Tabla-registro");
+const  Eregistro = document.getElementById("liveAlertBtn");
+const  Pregistro = document.getElementById("reg-form");
+const  Pregistro2 = document.getElementById("reg-form2");
+const  infodata = document.getElementById("Info-nodata");
+const  formFrutareg = document.getElementById("infofrutaFormReg");
+const  formClientereg = document.getElementById("infoClientFormReg");
+const  AlertPlaceholder = document.getElementById("liveAlertPlaceholder");
 
 var lista_frutas = [];
 
@@ -22,11 +25,17 @@ let agregarFecha = () =>{
 } 
 
 
-function crearFruta(id, nombre,costo){
+function crearFruta(form1,form2){
 	const fruta = {
-        id: id,
-	    nombre:nombre,  
-	    costo:costo, 
+
+        id: form1.get("idfrutreg"),
+	    namefruit:form1.get("namefrutareg"),
+	    Unitcost:form1.get("unitCostreg"),
+        amount: form1.get("cantidadreg"),
+        totalCost:form1.get("totalCostreg"),
+        nameClient: form2.get("nameclientreg"),
+        idClient:form2.get("idclientreg"),
+        seller:form2.get("vendedorreg"),
         fecha:agregarFecha()
     };
     return fruta
@@ -35,9 +44,53 @@ function crearFruta(id, nombre,costo){
 //Permite crear y agregar un objeto a la lista
 function agregarFruta(fruta){  
     lista_frutas.unshift(fruta);
+
 }
 
+function registrarFruta(form1, form2){
 
+    let fruta = crearFruta(form1,form2);
+    agregarFruta(fruta)
+}
+
+formClientereg.addEventListener("submit", function(event){
+                event.preventDefault();
+                let form1 = new FormData(formFrutareg);
+                let form2 = new FormData(formClientereg);
+                registrarFruta(form1,form2);
+                alert("La venta ha sido registrada exitosamente" ,"success");
+                Pregistro.style.display="none";
+                cargarFrutas();
+                formClientereg.remove;
+                formFrutareg.remove;
+                
+            }
+        )
+
+
+function alert(message, type) {
+    var wrapper = document.createElement('div')
+        wrapper.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" style="display: none;">
+        <symbol id="check-circle-fill" fill="currentColor" viewBox="0 0 16 16">
+          <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zm-3.97-3.03a.75.75 0 0 0-1.08.022L7.477 9.417 5.384 7.323a.75.75 0 0 0-1.06 1.06L6.97 11.03a.75.75 0 0 0 1.079-.02l3.992-4.99a.75.75 0 0 0-.01-1.05z"/>
+        </symbol>
+        <symbol id="info-fill" fill="currentColor" viewBox="0 0 16 16">
+          <path d="M8 16A8 8 0 1 0 8 0a8 8 0 0 0 0 16zm.93-9.412-1 4.705c-.07.34.029.533.304.533.194 0 .487-.07.686-.246l-.088.416c-.287.346-.92.598-1.465.598-.703 0-1.002-.422-.808-1.319l.738-3.468c.064-.293.006-.399-.287-.47l-.451-.081.082-.381 2.29-.287zM8 5.5a1 1 0 1 1 0-2 1 1 0 0 1 0 2z"/>
+        </symbol>
+        <symbol id="exclamation-triangle-fill" fill="currentColor" viewBox="0 0 16 16">
+          <path d="M8.982 1.566a1.13 1.13 0 0 0-1.96 0L.165 13.233c-.457.778.091 1.767.98 1.767h13.713c.889 0 1.438-.99.98-1.767L8.982 1.566zM8 5c.535 0 .954.462.9.995l-.35 3.507a.552.552 0 0 1-1.1 0L7.1 5.995A.905.905 0 0 1 8 5zm.002 6a1 1 0 1 1 0 2 1 1 0 0 1 0-2z"/>
+        </symbol>
+      </svg>
+      <div class="alert alert-${type} d-flex align-items-center" role="alert">
+        <svg class="bi flex-shrink-0 me-2" width="24" height="24" role="img" aria-label="Success:"><use xlink:href="#check-circle-fill"/></svg>
+        <div>
+          ${message}
+          <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+        </div>
+      </div>`
+        
+        //AlertPlaceholder.append(wrapper)
+    }
 // Permite eliminar un elemento de la lista cuyo identificador
 // corresponda al pasado por parámetro. 
 
@@ -95,20 +148,23 @@ function cargarFrutas(){
     if(lista_frutas.length>0){
         infodata.style.display = "none";
         for(let fruta in lista_frutas ){  
-            let newlistaRow = Tregistro.insertRow(-1);
+            let newlistaRow = Tregistro.insertRow(1);
             let newlistaCell = newlistaRow.insertCell(0);
             newlistaCell.textContent = lista_frutas[fruta].id
 
             newlistaCell = newlistaRow.insertCell(1);
-            newlistaCell.textContent = lista_frutas[fruta].nombre
+            newlistaCell.textContent = lista_frutas[fruta].nameClient
 
             newlistaCell = newlistaRow.insertCell(2);
-            newlistaCell.textContent = lista_frutas[fruta].costo
+            newlistaCell.textContent = lista_frutas[fruta].seller
 
             newlistaCell = newlistaRow.insertCell(3);
+            newlistaCell.textContent = lista_frutas[fruta].totalCost
+
+            newlistaCell = newlistaRow.insertCell(4);
             newlistaCell.textContent = lista_frutas[fruta].fecha
 
-            let optionsCell = newlistaRow.insertCell(4);
+            let optionsCell = newlistaRow.insertCell(5);
             optionsCell.insertAdjacentHTML ("beforeend", `<div class="dropdown">
             <button class="btn  bg-white" type="button" id="dropdownMenuButton1" data-bs-toggle="dropdown" aria-expanded="false">
                 <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-three-dots" viewBox="0 0 16 16">
