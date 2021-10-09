79a3ae0a9559f71483d90cea1abcf03225cd83b3
Armando Serna <aa.serna10@uniandes.edu.co>
2021-10-09 01:39:57 -0500

    Se agregaron los mensajes de confirmación

--- pruebas.js
+++ pruebas.js
@@ -22,7 +22,7 @@ const  formClientedit = document.getElementById("infoClientFormEdit");
 
 
 // panel Confirmaciones
-const  AlertPlaceholder = document.getElementById("liveAlertPlaceholder");
+ 
 
 
 const BtnBuscar = document.getElementById("buscarBtn1")
@@ -63,7 +63,8 @@ function crearFruta(form1,form2 ,type){
             fecha:agregarFecha()
         };
     }
-	 
+	
+     
     return fruta
 }
 
@@ -78,6 +79,7 @@ function registrarFruta(Form1,Form2){
     let fruta = crearFruta(Form1,Form2,1)
     lista_frutas.unshift(fruta)
     insertarFruta(fruta)
+    swal("Muy bien!", "El registro se guardó correctamente!", "success");
 }
 
 
@@ -212,29 +214,7 @@ formClientereg.addEventListener("submit", function(event){
         )
 
 // Función que permite ver la confirmación del registro  o de la edicción de la fruta
-function alert(message, type) {
-    var wrapper = document.createElement('div')
-        wrapper.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" style="display: none;">
-        <symbol id="check-circle-fill" fill="currentColor" viewBox="0 0 16 16">
-            <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zm-3.97-3.03a.75.75 0 0 0-1.08.022L7.477 9.417 5.384 7.323a.75.75 0 0 0-1.06 1.06L6.97 11.03a.75.75 0 0 0 1.079-.02l3.992-4.99a.75.75 0 0 0-.01-1.05z"/>
-        </symbol>
-        <symbol id="info-fill" fill="currentColor" viewBox="0 0 16 16">
-            <path d="M8 16A8 8 0 1 0 8 0a8 8 0 0 0 0 16zm.93-9.412-1 4.705c-.07.34.029.533.304.533.194 0 .487-.07.686-.246l-.088.416c-.287.346-.92.598-1.465.598-.703 0-1.002-.422-.808-1.319l.738-3.468c.064-.293.006-.399-.287-.47l-.451-.081.082-.381 2.29-.287zM8 5.5a1 1 0 1 1 0-2 1 1 0 0 1 0 2z"/>
-        </symbol>
-        <symbol id="exclamation-triangle-fill" fill="currentColor" viewBox="0 0 16 16">
-            <path d="M8.982 1.566a1.13 1.13 0 0 0-1.96 0L.165 13.233c-.457.778.091 1.767.98 1.767h13.713c.889 0 1.438-.99.98-1.767L8.982 1.566zM8 5c.535 0 .954.462.9.995l-.35 3.507a.552.552 0 0 1-1.1 0L7.1 5.995A.905.905 0 0 1 8 5zm.002 6a1 1 0 1 1 0 2 1 1 0 0 1 0-2z"/>
-        </symbol>
-      </svg>
-      <div class="alert alert-${type} d-flex align-items-center" role="alert">
-        <svg class="bi flex-shrink-0 me-2" width="24" height="24" role="img" aria-label="Success:"><use xlink:href="#check-circle-fill"/></svg>
-        <div>
-          ${message}
-          <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
-        </div>
-      </div>`
-        
-        //AlertPlaceholder.append(wrapper)
-    }
+
 
 
 // Funciones Editar info  -----------------------------------------------------------------------------------------------------------------------------------------
@@ -243,8 +223,18 @@ function alert(message, type) {
 // corresponda al pasado por parámetro. 
 
 function eliminarFruta(id){
-    let ans = confirm("Está seguro que desea eliminar la fruta seleccionada?");
-    if (ans == true){for (let fruta in lista_frutas){
+
+    swal({
+        title: "Estás seguro?",
+        text: "Una vez eliminado, no podrás recuperar el registro!",
+        icon: "warning",
+        buttons: true,
+        dangerMode: true,
+      })
+      .then((willDelete) => {
+        if (willDelete) {
+            
+    for (let fruta in lista_frutas){
         //console.log(fruta )
         if (lista_frutas[fruta].id == id){
             lista_frutas.splice(fruta, 1);
@@ -253,8 +243,14 @@ function eliminarFruta(id){
             }
         }
     }
-    document.getElementById(`delete-${id }`).parentNode.parentNode.parentNode.parentNode.parentNode.remove()    }
+    document.getElementById(`delete-${id }`).parentNode.parentNode.parentNode.parentNode.parentNode.remove()     
 
+          swal("Poof! El registro ha sido eliminado!", {
+            icon: "success",
+          });
+        } 
+      });
+     
 }
 
  // Permite remplazar un elemento de la lista cuyo identificador
@@ -274,6 +270,7 @@ function remplazarFruta(form3,form4){
     
     document.getElementById(`delete-${fruta.id }`).parentNode.parentNode.parentNode.parentNode.parentNode.remove();
     insertarFruta(fruta);
+    //swal("Muy bien!", "El registro se modificó correctamente!", "success");
     Peditar.style.display = "none";
 
 } 
@@ -338,10 +335,25 @@ function cancelar(){
 formClientedit.addEventListener("submit", function(event){
 
     event.preventDefault();
-    let ans = confirm("¿Esta seguró que desea continuar?")
-    if (ans == true){let form3 = new FormData(formFrutaedit);
+
+    swal({
+        title: "Estás seguro?",
+         
+        icon: "warning",
+        buttons: true,
+        dangerMode: true,
+      })
+      .then((willedit) => {
+        if (willedit) {
+        let form3 = new FormData(formFrutaedit);
         let form4 = new FormData(formClientedit);   
-        remplazarFruta(form3,form4);}
+        remplazarFruta(form3,form4);
+          swal("El registro ha sido modificado!", {
+            icon: "success",
+          });
+        }  
+      });
+    
     
                     
     
